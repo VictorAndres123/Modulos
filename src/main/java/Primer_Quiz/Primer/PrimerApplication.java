2175602963fe4a568505aed350a1b7b6b93f0d48
VictorAndres123 <elcangasomg@gmail.com>
2023-05-04 21:28:26 -0500

    Proyecto SpringBoot

---
+++ src/main/java/Primer_Quiz/Primer/PrimerApplication.java
@@ -0,0 +1,13 @@
+package Primer_Quiz.Primer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerApplication.class, args);
+	}
+
+}
